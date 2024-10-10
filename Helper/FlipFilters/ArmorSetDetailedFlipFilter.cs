c97a9856e519b59aa0f6552e9b96be5bd0eddcf3
Äkwav <ekwav@coflnet.com>
2024-10-10 14:04:47 +0200

    Add seymour armor set to filter fix #65

--- Helper/FlipFilters/ArmorSetDetailedFlipFilter.cs
+++ Helper/FlipFilters/ArmorSetDetailedFlipFilter.cs
@@ -12,8 +12,13 @@ public class ArmorSetDetailedFlipFilter : DetailedFlipFilter
 {
     public object[] Options => ItemDetails.Instance.TagLookup
         .Where(t => t.Key.EndsWith("_LEGGINGS") && IsOnAh(t))
-        .Select(t => (object)t.Key.Replace("_LEGGINGS", "")).ToArray();
+        .Select(t => (object)t.Key.Replace("_LEGGINGS", "")).Append(ExtraArmorSets.Keys).ToArray();
 
+
+    private static Dictionary<string, string[]> ExtraArmorSets = new()
+    {
+        { "seymour", new []{ "OXFORD_SHOES", "SATIN_TROUSERS", "CASHMERE_JACKET", "VELVET_TOP_HAT" } },
+    };
     private static bool IsOnAh(KeyValuePair<string, int> t)
     {
         return !t.Key.Contains("_TERROR_") && !t.Key.Contains("_CRIMSON_") && !t.Key.Contains("_AURORA_") && !t.Key.Contains("_FERVOR_") && !t.Key.Contains("_HOLLOW_");
@@ -23,6 +28,8 @@ public class ArmorSetDetailedFlipFilter : DetailedFlipFilter
 
     public Expression<Func<FlipInstance, bool>> GetExpression(FilterContext filters, string val)
     {
+        if (ExtraArmorSets.TryGetValue(val, out var set))
+            return flip => set.Contains(flip.Auction.Tag);
         var regex = new Regex('^' + Regex.Escape(val) + "_(CHESTPLATE|HELMET|BOOTS|LEGGINGS)");
         return flip => regex.Match(flip.Auction.Tag).Success;
     }
