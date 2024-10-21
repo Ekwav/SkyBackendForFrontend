43ff21b8d9239b9ce9b065c6712baa93634803d1
Äkwav <ekwav@coflnet.com>
2024-10-21 17:07:38 +0200

    Add test to attest fix #69

--- Services/MuseumService.Tests.cs
+++ Services/MuseumService.Tests.cs
@@ -58,6 +58,33 @@ public class MuseumServiceTests
         result.First().Value.auctionid.Length.Should().Be(2);
     }
 
+    [Test]
+    public async Task ExcludesChildIfParentDonated()
+    {
+        var auctionMock = new Mock<IAuctionApi>();
+        var itemService = new Mock<IHypixelItemStore>();
+        itemService.Setup(service => service.GetItemsAsync()).ReturnsAsync(() => new Dictionary<string, Core.Services.Item>(){
+            {"SKYMART_VACUUM", new CopyAble(baseItem){Id="SKYMART_VACUUM", MuseumData=new(2, "RARITIES", new(){{"SKYMART_VACUUM","SKYMART_TURBO_VACUUM"}}, null, null, "AMATEUR")}},
+            {"SKYMART_TURBO_VACUUM", new CopyAble(baseItem){Id="SKYMART_TURBO_VACUUM",MuseumData=new(2, "RARITIES", new(){{"SKYMART_TURBO_VACUUM","SKYMART_HYPER_VACUUM"}}, null, null, "AMATEUR")}},
+            {"SKYMART_HYPER_VACUUM", new CopyAble(baseItem){Id="SKYMART_HYPER_VACUUM",MuseumData=new(4, "RARITIES", new(){{"SKYMART_HYPER_VACUUM","INFINI_VACUUM"}}, null, null, "INTERMEDIATE")}},
+            {"INFINI_VACUUM", new CopyAble(baseItem){Id="INFINI_VACUUM",MuseumData=new(6, "RARITIES", new(){{"INFINI_VACUUM","INFINI_VACUUM_HOOVERIUS"}}, null, null, "SKILLED")}},
+            {"INFINI_VACUUM_HOOVERIUS", new CopyAble(baseItem){Id="INFINI_VACUUM_HOOVERIUS",MuseumData=new(8, "RARITIES", new(), null, null, "EXPERT")} }
+        });
+        auctionMock.Setup(service => service.ApiAuctionLbinsGetAsync(0, default)).ReturnsAsync(() => new Dictionary<string, Sniper.Client.Model.ReferencePrice>(){
+            {"SKYMART_VACUUM", new (){Price=100, AuctionId=1234}},
+            {"SKYMART_TURBO_VACUUM", new (){Price=100, AuctionId=1235}},
+            {"SKYMART_HYPER_VACUUM", new (){Price=100, AuctionId=1235}},
+            {"INFINI_VACUUM", new (){Price=100, AuctionId=1235}},
+            {"INFINI_VACUUM_HOOVERIUS", new (){Price=100, AuctionId=1235}}
+        });
+        var service = new MuseumService(auctionMock.Object, NullLogger<MuseumService>.Instance, itemService.Object);
+        var result = await service.GetBestOptions(new HashSet<string>(), 30);
+        result.Count.Should().Be(5);
+
+        result = await service.GetBestOptions(new HashSet<string>() { "INFINI_VACUUM_HOOVERIUS" }, 30);
+        result.Count.Should().Be(0);
+    }
+
     public record CopyAble : Core.Services.Item
     {
         public CopyAble(Core.Services.Item item) : base(item)
